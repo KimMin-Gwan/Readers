6a77276aee80f878cd7b5fd72d72eef231dfca84
BBangGy <124778694+BBangGy@users.noreply.github.com>
2023-12-01 04:56:57 +0900

    Update sellingInfo.dart
    서버 추가후 수정

--- Project/main/readers/lib/sellingInfo.dart
+++ Project/main/readers/lib/sellingInfo.dart
@@ -1,122 +1,160 @@
 import 'package:flutter/material.dart';
 import './reviewPage.dart';
 
-class SellingInfo extends StatelessWidget {
+class SellingInfo extends StatefulWidget {
   const SellingInfo({super.key});
+
+  @override
+  State<SellingInfo> createState() => _SellingInfoState();
+}
+
+class _SellingInfoState extends State<SellingInfo> {
+
+  var sellInfoData = [];
+
+  getData() async {
+    var result = await http.get(Uri.http('127.0.0.1:8000', '/homePage'));
+    if (result.statusCode == 200) {
+      print(utf8.decode(result.bodyBytes));
+      setState(() {
+        this.sellInfoData = jsonDecode(utf8.decode(result.bodyBytes));
+      });
+      print( this.sellInfoData );
+    } else {
+      throw Exception('Data Fail to Get');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home : Scaffold(
+    print(this.sellInfoData);
+    return SubSellInfo( sellInfoData :this.sellInfoData);
+  }
+}
 
+class SubSellInfo extends StatelessWidget {
+  const SubSellInfo({super.key, this.sellInfoData});
+  final sellInfoData;
 
-        body:Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(child: Container(height: 300,
-                color: Colors.white70,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+  @override
+  Widget build(BuildContext context) {
+    print(sellInfoData);
+    if(this.sellInfoData.isNotEmpty){
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(child: Container(height: 300,
+            color: Colors.white70,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('camera.jpeg'),
+                VerticalDivider(thickness: 3,color: Colors.black,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image.asset('camera.jpeg'),
-                    VerticalDivider(thickness: 3,color: Colors.black,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Spacer(),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text('출판사',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
-                            Container(
-                              margin: EdgeInsets.only(left: 8),
-                              child: Text('우리집'),
+                        Text("출판사",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+                        Container(
+                          margin: EdgeInsets.only(left: 8),
+                          child: Text(this.sellInfoData["publisher"]),
 
-                            )
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text('제목',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                            Container(
-                              margin: EdgeInsets.only(left: 8),
-                              child:Text('너의 이름은 뭐니?')
-                            )
-                          ],
-                        ),
-                        Spacer(),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Icon(Icons.perm_identity_rounded),
-                            Text('저자',style: TextStyle(fontSize: 10)),
-                            Container(
-                              margin: EdgeInsets.only(left: 8),
-                              child: Text('정현'),
-                            )
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Icon(Icons.reviews),
-                            Text('리뷰',style:TextStyle(fontSize: 10)),
-                            Container(
-                              margin: EdgeInsets.only(left: 8), // 원하는 간격으로 조절
-                              child: ElevatedButton(
-                                onPressed: () {},
-                                child: Text('상세 보기'),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Icon(Icons.date_range),
-                            Text('날짜',style:TextStyle(fontSize: 10)),
-                            Container(
-                              margin: EdgeInsets.only(left: 8), // 원하는 간격으로 조절
-                              child: ElevatedButton(
-                                onPressed: () {
-                                   Navigator.push(context, MaterialPageRoute(builder: (context)=>const ReviewPage()));
-                                },
-                                child: Text('리뷰 보기'),
-                              ),
-                            ),
-                          ],
-                        ),
+                        )
                       ],
-                    )
-                  ],
-                ),
-              ),),
-              Divider(thickness: 3,color: Colors.black,),
-              Container(
-                  decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-                  child: ListView(
-                  shrinkWrap: true,
-                  children: [
-                    ListTile(title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Text('교보문고'),
-                        Text('8000원')
+                        Text("제목",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                        Container(
+                            margin: EdgeInsets.only(left: 8),
+                            child:Text(this.sellInfoData["title"])
+                        )
                       ],
-                    ),),
-                    ListTile(title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Text('교보문고'),
-                        Text('8000원')
+                        Icon(Icons.perm_identity_rounded),
+                        Text('저자',style: TextStyle(fontSize: 10)),
+                        Container(
+                          margin: EdgeInsets.only(left: 8),
+                          child: Text(this.sellInfoData["author"]),
+                        )
                       ],
-                    ),),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(Icons.reviews),
+                        Text('리뷰',style:TextStyle(fontSize: 10)),
+                        Container(
+                          margin: EdgeInsets.only(left: 8), // 원하는 간격으로 조절
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text('상세 보기'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(Icons.date_range),
+                        Text('날짜',style:TextStyle(fontSize: 10)),
+                        Container(
+                          margin: EdgeInsets.only(left: 8), // 원하는 간격으로 조절
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>const ReviewPage()));
+                            },
+                            child: Text('리뷰 보기'),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
-                )),
-            ],
-        )
-      )
-    );
+                )
+              ],
+            ),
+          ),),
+          Divider(thickness: 3,color: Colors.black,),
+          Container(
+              decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(this.sellInfoData["purchaseLink"]),
+                      Text('8000원')
+                    ],
+                  ),),
+                  ListTile(title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('교보문고'),
+                      Text('8000원')
+                    ],
+                  ),),
+                ],
+              )),
+        ],
+      );
+    }else{
+      return Text("로딩중");
+    }
   }
 }
 
